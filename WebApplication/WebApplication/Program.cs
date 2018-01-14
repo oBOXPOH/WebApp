f7c0cda0d394cb6189b0c14316c13327d98fd40b
oBOXPOH <rc_38@mail.ru>
2018-01-14 18:46:01 +0300

    First change of project

---
+++ WebApplication/WebApplication/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var host = WebHost.Start("http://localhost:8080", context => context.Response.WriteAsync("Hello!")))
+            {
+                Console.WriteLine("Server has been started!");
+                host.WaitForShutdown();
+            }
+
+            //var host = new WebHostBuilder()
+            //    .UseKestrel()
+            //    .UseContentRoot(Directory.GetCurrentDirectory())
+            //    .UseIISIntegration()
+            //    .UseStartup<Startup>()
+            //    .Build();
+
+            //host.Run();
+
+            //BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ WebApplication/WebApplication/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApplication
+{
+    public class Startup
+    {
+        public Startup() // Constructor for initialization of needed parameters
+        {
+
+        }
+
+        public void ConfigureServices(IServiceCollection services) // Addition of services
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) // Processing of requests
+        {
+            int x = 0;
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseMvc();
+
+            app.Run(async (context) =>
+            {
+                x++;
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
