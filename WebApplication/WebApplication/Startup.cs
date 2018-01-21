a7c51ab68715d4961b33e7466fb0b2ca16d30974
oBOXPOH <rc_38@mail.ru>
2018-01-21 23:16:07 +0300

    Real preparing for Web Project

--- WebApplication/WebApplication/Startup.cs
+++ WebApplication/WebApplication/Startup.cs
@@ -24,35 +24,20 @@ namespace WebApplication
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) // Processing of requests
         {
-            int x = 0;
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseStaticFiles();
-            app.UseMvc();
-            app.UseStaticFiles();
-            app.UseMiddleware<AuthenticationMiddleware>();
-            app.UseMiddleware<RoutingMiddleware>();
 
-            app.Map("/index", (appBuilder) =>
-            {
-                app.Run(async (context) =>
-                {
-                    await context.Response.WriteAsync("It's INDEX");
-                });
-            });
-
-            app.Run(async (context) =>
-            {
-                x++;
-                string host = context.Request.Host.Value;
-                string path = context.Request.Path;
-                string query = context.Request.QueryString.Value;
-                await context.Response.WriteAsync($"<h1>{host}</h1><h2>{path}</h2><h3>{query}</h3>");
-            });
+            app.UseMvcWithDefaultRoute();
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
         }
     }
 }
