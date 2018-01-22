433f33f1953fd86b7b7936bfb3f9d0ac90473465
oBOXPOH <rc_38@mail.ru>
2018-01-22 22:38:27 +0300

    Fixed header and menu, added wrapper for both

--- WebApplication/WebApplication/Startup.cs
+++ WebApplication/WebApplication/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using WebApplication.Middleware;
 
@@ -20,6 +21,7 @@ namespace WebApplication
         public void ConfigureServices(IServiceCollection services) // Addition of services
         {
             services.AddMvc();
+            services.AddRouting();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) // Processing of requests
@@ -32,12 +34,23 @@ namespace WebApplication
             app.UseStaticFiles();
 
             app.UseMvcWithDefaultRoute();
+
+            var routeHandler = new RouteHandler(Handle);
+            var routeBuilder = new RouteBuilder(app, routeHandler);
+            routeBuilder.MapRoute("extended", "NewRoute/{controller}/{action}/{id?}");
+            app.UseRouter(routeBuilder.Build());
+
             //app.UseMvc(routes =>
             //{
             //    routes.MapRoute(
             //        name: "default",
             //        template: "{controller=Home}/{action=Index}/{id?}");
             //});
+        }
+
+        public async Task Handle(HttpContext context)
+        {
+            await context.Response.WriteAsync("ROUTE!");
         }
     }
 }
