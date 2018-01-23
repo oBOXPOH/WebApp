dc88958b417d53d33253b829d7af24a1a887c8d2
oBOXPOH <rc_38@mail.ru>
2018-01-23 23:58:59 +0300

    New update!

--- WebApplication/WebApplication/Startup.cs
+++ WebApplication/WebApplication/Startup.cs
@@ -6,20 +6,35 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using WebApplication.Middleware;
+using WebApplication.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace WebApplication
 {
     public class Startup
     {
-        public Startup() // Constructor for initialization of needed parameters
-        {
+        public IConfiguration Configuration { get; }
 
+        public Startup(IConfiguration configuration) // Constructor for initialization of needed parameters
+        {
+            Configuration = configuration;
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
         }
 
         public void ConfigureServices(IServiceCollection services) // Addition of services
         {
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationContext>();
+
             services.AddMvc();
             services.AddRouting();
         }
@@ -33,24 +48,25 @@ namespace WebApplication
 
             app.UseStaticFiles();
 
+            app.UseAuthentication();
+
             app.UseMvcWithDefaultRoute();
-
-            var routeHandler = new RouteHandler(Handle);
-            var routeBuilder = new RouteBuilder(app, routeHandler);
-            routeBuilder.MapRoute("extended", "NewRoute/{controller}/{action}/{id?}");
-            app.UseRouter(routeBuilder.Build());
-
             //app.UseMvc(routes =>
             //{
             //    routes.MapRoute(
             //        name: "default",
             //        template: "{controller=Home}/{action=Index}/{id?}");
             //});
+
+            //var routeHandler = new RouteHandler(Handle);
+            //var routeBuilder = new RouteBuilder(app, routeHandler);
+            //routeBuilder.MapRoute("extended", "NewRoute/{controller}/{action}/{id?}");
+            //app.UseRouter(routeBuilder.Build());
         }
 
-        public async Task Handle(HttpContext context)
-        {
-            await context.Response.WriteAsync("ROUTE!");
-        }
+        //public async Task Handle(HttpContext context)
+        //{
+        //    await context.Response.WriteAsync("ROUTE!");
+        //}
     }
 }
